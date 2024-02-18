c67dceac8b58338d761615fd55641fba9284b114
Sohanuzzman Soad <mdsoad@gmail.com>
2024-02-19 02:40:55 +0600

    Update API endpoint URLs

--- lib/data/datasources/remote/apis/auth_api.dart
+++ lib/data/datasources/remote/apis/auth_api.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'dart:io';
 
 import 'package:get/get.dart';
@@ -8,96 +10,19 @@ import '../api_endpoint.dart';
 import '../core/api_provider.dart';
 import '../core/api_request.dart';
 
-
-enum AuthType {
-  register,
-  login,
-  socialLogin,
-  logout,
-  currentUser,
-  forgotPasswordSendEmail,
-  forgotPasswordVerifyCode,
-  checkEmailExist,
-  checkPhoneExist,
-  setNewPassword,
-  deleteUser,
+enum RequestType {
+  GetProductList,
+  GetProductDetails,
 }
 
-enum SocialProvider { google, facebook, twitter, apple }
+class ProductAPI implements IHTTPRequest {
+  final RequestType type;
 
-class AuthAPI implements IHTTPRequest {
-  final AuthType type;
-  SocialProvider? socialProvider;
-  String? id;
-  String? name;
-  String? callingCode;
-  String? phone;
-  String? email;
-  String? photoUrl;
-  String? password;
-  String? cPassword;
-  String? code;
+  ProductAPI._({required this.type});
 
-  AuthAPI._(
-      {required this.type,
-      this.socialProvider,
-      this.id,
-      this.password,
-      this.name,
-      this.email,
-      this.photoUrl,
-      this.callingCode,
-      this.phone,
-      this.code,
-      this.cPassword});
+  ProductAPI.getProductList() : this._(type: RequestType.GetProductList);
 
-  AuthAPI.checkEmailExist(String email)
-      : this._(type: AuthType.checkEmailExist, email: email);
-
-  AuthAPI.checkPhoneExist(String phone)
-      : this._(type: AuthType.checkPhoneExist, phone: phone);
-
-  AuthAPI.login(String username, String password)
-      : this._(type: AuthType.login, email: username, password: password);
-
-  AuthAPI.register(String name, String email, String callingCode, String phone,
-      String password, String cPassword)
-      : this._(
-            type: AuthType.register,
-            name: name,
-            email: email,
-            callingCode: callingCode,
-            phone: phone,
-            password: password,
-            cPassword: cPassword);
-
-  AuthAPI.socialLogin(String id, String name, String email, String photoUrl,
-      SocialProvider socialProvider)
-      : this._(
-            type: AuthType.socialLogin,
-            socialProvider: socialProvider,
-            id: id,
-            name: name,
-            email: email,
-            photoUrl: photoUrl);
-
-  AuthAPI.logout() : this._(type: AuthType.logout);
-
-  AuthAPI.forgotPassword(String email)
-      : this._(type: AuthType.forgotPasswordSendEmail, email: email);
-
-  AuthAPI.forgotPasswordVerifyCode(String email, String code)
-      : this._(
-            type: AuthType.forgotPasswordVerifyCode, email: email, code: code);
-  AuthAPI.currentUser() : this._(type: AuthType.currentUser);
-
-  AuthAPI.setNewPassword(String email, String password, String cPassword)
-      : this._(
-            type: AuthType.setNewPassword,
-            email: email,
-            password: password,
-            cPassword: cPassword);
-  AuthAPI.deleteUser() : this._(type: AuthType.deleteUser);
+  ProductAPI.getProductDetails() : this._(type: RequestType.GetProductDetails);
 
   @override
   String get endpoint => APIEndpoint.BASE_URL;
@@ -105,39 +30,10 @@ class AuthAPI implements IHTTPRequest {
   @override
   String get path {
     switch (type) {
-      case AuthType.checkEmailExist:
-        return APIEndpoint.CHECK_EMAIL_EXIST;
-      case AuthType.checkPhoneExist:
-        return APIEndpoint.CHECK_PHONE_EXIST;
-      case AuthType.register:
-        return APIEndpoint.SIGN_UP;
-      case AuthType.login:
-        return APIEndpoint.LOGIN;
-      case AuthType.forgotPasswordSendEmail:
-        return APIEndpoint.FORGOT_PASSWORD_SEND_EMAIL;
-      case AuthType.forgotPasswordVerifyCode:
-        return APIEndpoint.FORGOT_PASSWORD_VERIFY_CODE;
-      case AuthType.setNewPassword:
-        return APIEndpoint.SET_NEW_PASSWORD;
-      case AuthType.socialLogin:
-        switch (socialProvider) {
-          case SocialProvider.google:
-            return APIEndpoint.SOCIAL_LOGIN_GOOGLE;
-          case SocialProvider.facebook:
-            return APIEndpoint.SOCIAL_LOGIN_FACEBOOK;
-          // case SocialProvider.twitter:
-          //   return APIEndpoint.TWITTER_LOGIN;
-          case SocialProvider.apple:
-            return APIEndpoint.SOCIAL_LOGIN_APPLE;
-          default:
-            return "";
-        }
-      case AuthType.logout:
-        return APIEndpoint.LOGOUT;
-      case AuthType.currentUser:
-        return APIEndpoint.GET_PROFILE;
-      case AuthType.deleteUser:
-        return APIEndpoint.DELETE_USER;
+      case RequestType.GetProductList:
+        return APIEndpoint.PRODUCT_LIST;
+      case RequestType.GetProductDetails:
+        return APIEndpoint.PRODUCT_DETAILS;
       default:
         return "";
     }
@@ -146,28 +42,9 @@ class AuthAPI implements IHTTPRequest {
   @override
   HTTPMethod get method {
     switch (type) {
-      case AuthType.checkEmailExist:
-        return HTTPMethod.post;
-      case AuthType.checkPhoneExist:
-        return HTTPMethod.post;
-      case AuthType.register:
-        return HTTPMethod.post;
-      case AuthType.login:
-        return HTTPMethod.post;
-      case AuthType.socialLogin:
-        return HTTPMethod.post;
-      case AuthType.logout:
-        return HTTPMethod.post;
-      case AuthType.forgotPasswordSendEmail:
-        return HTTPMethod.post;
-      case AuthType.forgotPasswordVerifyCode:
-        return HTTPMethod.post;
-      case AuthType.setNewPassword:
-        return HTTPMethod.post;
-      case AuthType.currentUser:
+      case RequestType.GetProductList:
+      case RequestType.GetProductDetails:
         return HTTPMethod.get;
-      case AuthType.deleteUser:
-        return HTTPMethod.post;
       default:
         return HTTPMethod.get;
     }
@@ -176,27 +53,9 @@ class AuthAPI implements IHTTPRequest {
   @override
   Map<String, String> get headers {
     switch (type) {
-      case AuthType.currentUser:
-        return {
-          HttpHeaders.contentTypeHeader: 'application/json',
-          HttpHeaders.authorizationHeader:
-              'Bearer ${Get.find<LocalStorageService>().apiToken!}',
-          HttpHeaders.acceptHeader: 'application/json',
-        };
-
-      case AuthType.logout:
-        return {
-          HttpHeaders.contentTypeHeader: 'application/json',
-          HttpHeaders.authorizationHeader:
-              'Bearer ${Get.find<LocalStorageService>().apiToken!}'
-        };
-
-      case AuthType.deleteUser:
-        return {
-          HttpHeaders.contentTypeHeader: 'application/json',
-          HttpHeaders.authorizationHeader:
-              'Bearer ${Get.find<LocalStorageService>().apiToken!}'
-        };
+      case RequestType.GetProductList:
+      case RequestType.GetProductDetails:
+        return {HttpHeaders.contentTypeHeader: 'application/json'};
       default:
         return {HttpHeaders.contentTypeHeader: 'application/json'};
     }
@@ -204,56 +63,24 @@ class AuthAPI implements IHTTPRequest {
 
   @override
   Map<String, String> get query {
-    return {HttpHeaders.contentTypeHeader: 'application/json'};
+    switch (type) {
+      case RequestType.GetProductList:
+        return {};
+      case RequestType.GetProductDetails:
+        return {
+          "id": "1",
+        };
+      default:
+        return {};
+    }
   }
 
   @override
   get body {
     switch (type) {
-      case AuthType.checkEmailExist:
-        return {"email": email};
-
-      case AuthType.checkPhoneExist:
-        return {"phone": phone};
-
-      case AuthType.register:
-        return {
-          "name": name,
-          "email": email,
-          "calling_code": callingCode,
-          "phone": phone,
-          "password": password,
-          "c_password": cPassword
-        };
-      case AuthType.login:
-        return {
-          "identity": email,
-          "password": password,
-          "device_token": Get.find<LocalStorageService>().firebaseDeviceToken!
-        };
-      case AuthType.socialLogin:
-        return {
-          "id": id,
-          "name": name,
-          "email": email,
-          "photoUrl": photoUrl,
-          "device_token": Get.find<LocalStorageService>().firebaseDeviceToken!
-        };
-      case AuthType.forgotPasswordSendEmail:
-        return {
-          "email": email,
-        };
-      case AuthType.forgotPasswordVerifyCode:
-        return {
-          "email": email,
-          "verfication_code": code,
-        };
-      case AuthType.setNewPassword:
-        return {
-          "email": email,
-          "password": password,
-          "confirm_password": cPassword,
-        };
+      case RequestType.GetProductList:
+      case RequestType.GetProductDetails:
+        return {};
       default:
         return {};
     }
